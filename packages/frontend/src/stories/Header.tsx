10e0fc59a9b2aa5a436127644a471ac249412784
Marcin Wozniak <persky03@gmail.com>
2023-02-23 21:53:46 +0100

    ci: linter not working;c

--- packages/frontend/src/stories/Header.tsx
+++ packages/frontend/src/stories/Header.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
   onLogin: () => void;
   onLogout: () => void;
   onCreateAccount: () => void;
-}
+};
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
   <header>
