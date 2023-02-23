d12691d02a8565ca9b34acc02a0054c1e67ca736
Marcin Wozniak <persky03@gmail.com>
2023-02-23 21:20:11 +0100

    build: implement tailwind and scss

---
+++ packages/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='max-w-full'>
+        <div className='bg-red-600' id="header">Test</div>
+    </div>
+  )
+}
