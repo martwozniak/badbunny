8f9a61771dfbac6c1ba1e3704c6c9ec08c4911ce
Marcin Wozniak <persky03@gmail.com>
2023-02-24 07:16:29 +0100

    feat: simple header

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
 export default function Header() {
   return (
-    <div className="max-w-full">
-      <div className="bg-red-600" id="header">
-        Test
+    <div className="bg-black px-4 py-4 min-h-[60px] flex justify-between items-center">
+      <div className="logo">
+        <span className="text-white text-xs">BadBunny</span>
+      </div>
+      <div className="menu">
+        <nav>
+          <ul className="text-white text-xs flex gap-2 items-center">
+            <li>Games</li>
+            <li>Prices</li>
+            <li className="CTA_Nav bg-green-600 px-4 py-2 rounded">
+              Claim 300 000 points
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
