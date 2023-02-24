0e624a5752189ff372566348faecd367250be3c9
Marcin Wozniak <persky03@gmail.com>
2023-02-24 08:14:19 +0100

    fix: header elements hover

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -10,14 +10,16 @@ export default function Header({ logo, backgroundColor, fontSize }: any) {
   return (
     <div className="bg-black px-4 py-4 min-h-[60px] flex justify-between items-center">
       <div className="logo">
-        <span className="text-white text-xs">BadBunny</span>
+        <span className="text-white text-xs cursor-pointer">
+          Bad<span className="font-bold">Bunny</span>
+        </span>
       </div>
       <div className="menu">
         <nav>
           <ul className="text-white text-xs flex gap-2 items-center">
-            <li>Games</li>
-            <li>Prices</li>
-            <li className="CTA_Nav bg-green-600 px-4 py-2 rounded">
+            <li className="cursor-pointer">Games</li>
+            <li className="cursor-pointer">Prices</li>
+            <li className="CTA_Nav bg-green-600 px-4 py-2 rounded cursor-pointer hover:bg-green-700 transition-all">
               Claim 300 000 points
             </li>
           </ul>
